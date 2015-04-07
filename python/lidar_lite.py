4850c3f647f4222bd21d8fcc0159905c1a9db5b2
Sander Idelson <sander.idelson@gmail.com>
2015-04-06 19:10:55 -0700

    Python Library created

---
+++ python/lidar_lite.py
@@ -0,0 +1,50 @@
+import smbus
+import time
+
+class Lidar_Lite():
+  def __init__(self):
+    self.address = 0x62
+    self.distWriteReg = 0x00
+    self.distWriteVal = 0x04
+    self.distReadReg1 = 0x8f
+    self.distReadReg2 = 0x98
+    self.velWriteReg = 0x04
+    self.velWriteVal = 0x08
+    self.velReadVal = 0x09
+
+  def connect(self, bus):
+    try:
+      self.bus = smbus.SMBus(bus)
+      time.sleep(0.5)
+      return 0
+    except:
+      return -1
+
+  def writeAndWait(self, register, value):
+    self.bus.write_byte_data(self.address, register, value);
+    time.sleep(0.02)
+
+  def readAndWait(self, register):
+    res = self.bus.read_byte_data(self.address, register)
+    time.sleep(0.02)
+    return res
+
+  def getDistance(self):
+    self.writeAndWait(self.distWriteReg, self.distWriteVal)
+    dist1 = self.readAndWait(self.distReadReg1)
+    dist2 = self.readAndWait(self.distReadReg2)
+    return (dist1 << 8) + dist2
+
+  def getVelocity(self):
+    self.writeAndWait(self.distWriteReg, self.distWriteVal)
+    self.writeAndWait(self.velWriteReg, self.velWriteVal)
+    vel = self.readAndWait(self.velReadReg)
+    return signedInt(vel)
+
+  def signedInt(value):
+    if value > 127:
+      return (256-value) * (-1)
+    else:
+      return value
+
+
---
+++ python/test.py
@@ -0,0 +1,9 @@
+from lidar_lite import Lidar_Lite
+
+lidar = Lidar_Lite()
+connected = lidar.connect(1)
+
+if connected < -1:
+  print "Not Connected"
+
+print lidar.getDistance()
